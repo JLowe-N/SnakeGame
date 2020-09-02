ae80b76f321bf314d43129d8203b1cc46f23ed54
Justin Lowen <Justin.G.Lowen@gmail.com>
2020-09-02 17:05:22 -0500

    Implementing additional move function - apple eat logic

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
-const score = document.getElementById('score')
+const scoreDisplay = document.getElementById('score')
 let squares = []
-let currentSnake = [2,1,0]
+let currentSnake = [2, 1, 0]
 let direction = 1
 const width = 10
 let appleIndex = 0
+let score = 0
+let intervalTime = 1000
+let speed = 0.5
 
 function createGrid() {
     //create 100 of these elements with a for loop
-    for (let i=0; i < width*width; i++) {
-     //create element
-    const square = document.createElement('div')
-    //add styling to the element
-    square.classList.add('square')
-    //put the element into our grid
-    grid.appendChild(square)
-    //push it into a new squares array    
-    squares.push(square)
+    for (let i = 0; i < width * width; i++) {
+        //create element
+        const square = document.createElement('div')
+        //add styling to the element
+        square.classList.add('square')
+        //put the element into our grid
+        grid.appendChild(square)
+        //push it into a new squares array    
+        squares.push(square)
     }
 }
 createGrid()
@@ -26,13 +29,13 @@ currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
     if (
-        (currentSnake[0] + width >= width*width && direction === width) || //if snake has hit bottom
-        (currentSnake[0] % width === width-1 && direction === 1) || //if snake has hit right wall
+        (currentSnake[0] + width >= width * width && direction === width) || //if snake has hit bottom
+        (currentSnake[0] % width === width - 1 && direction === 1) || //if snake has hit right wall
         (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
         (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
         squares[currentSnake[0] + direction].classList.contains('snake')
     )
-    return clearInterval(timerId)
+        return clearInterval(timerId)
 
     //remove last element from our currentSnake array
     const tail = currentSnake.pop()
@@ -41,32 +44,35 @@ function move() {
     //add square in direction we are heading
     currentSnake.unshift(currentSnake[0] + direction)
     //add styling so we can see it
-    
+
     //deal with snake head gets apple
     if (squares[currentSnake[0]].classList.contains('apple')) {
         //remove the class of apple
         squares[currentSnake[0]].classList.remove('apple')
         //grow our snake by adding class of snake to it
-        
+        squares[tail].classList.add('snake')
         //grow our snake array
-        
+        currentSnake.push(tail)
         //generate new apple
-        
+        generateApple()
         //add one to the score
-        
+        score++
         //display our score
-        
+        scoreDisplay.textContent = score
         //speed up our snake
-        
+        clearInterval(timerId)
+        console.log(intervalTime)
+        intervalTime = intervalTime * speed
+        timerId = setInterval(move, intervalTime)
     }
-    
-    
-    
+
+
+
     squares[currentSnake[0]].classList.add('snake')
 }
 move()
 
-let timerId = setInterval(move, 1000)
+let timerId = setInterval(move, intervalTime)
 
 
 
@@ -75,7 +81,7 @@ function generateApple() {
         appleIndex = Math.floor(Math.random() * squares.length)
     } while (squares[appleIndex].classList.contains('snake'))
     squares[appleIndex].classList.add('apple')
-} 
+}
 generateApple()
 
 // 39 is right arrow
