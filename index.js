9d9e316cf3959993274c4f4f3ebec0af469f9bb0
Justin Lowen <Justin.G.Lowen@gmail.com>
2020-09-02 16:09:11 -0500

    Build move function javascript and listeners

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
+let squares = []
+let currentSnake = [2, 1, 0]
+let direction = 1
 
 function createGrid() {
     // create elements
-
-    // create 100 of these elements
-
-    // add styling to these elements
-
-    // create array of squares
+    for (let i = 0; i < 100; i++) {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        grid.appendChild(square)
+        squares.push(square)
+    }
 }
+
+createGrid()
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
+
+function move() {
+    //remove last element from our currentSnake array
+    const tail = currentSnake.pop()
+    //remove styling from the last element
+    squares[tail].classList.remove('snake')
+    //add square in direction we are heading
+    currentSnake.unshift(currentSnake[0] + direction)
+    //add styling so we can see it
+    squares[currentSnake[0]].classList.add('snake')
+}
+
+move()
+
+let timerId = setInterval(move, 1000)
+
+
+document.addEventListener("keyup", (e) => {
+    if (e.keyCode === 39) {
+        direction = 1
+    } else if (e.keyCode === 38) {
+        direction = -10
+    } else if (e.keyCode === 37) {
+        direction = -1
+    } else if (e.keyCode === 40) {
+        direction = 10
+    }
+})
+// clearInterval(timerId)
