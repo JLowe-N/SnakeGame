df2933189275e86145f7dc95ac8a51bb413992bc
Justin Lowen <Justin.G.Lowen@gmail.com>
2020-09-02 16:19:49 -0500

    Check for gameover logic in move

--- index.js
+++ index.js
@@ -14,12 +14,27 @@ function createGrid() {
         squares.push(square)
     }
 }
-
 createGrid()
 
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
+    if (
+        //if snake has hit bottom
+        (currentSnake[0] + width >= 100 && direction === 10) ||
+        (currentSnake[0] % width === 9 && direction === 1) ||
+        (currentSnake[0] % width === 0 && direction === - 1) ||
+        (currentSnake[0] - width < 0 && direction === -10) ||
+        squares[currentSnake[0] + direction].classList.contains('snake')
+        //if snake has hit right wall
+        //if snake hast hit left wall
+        //if snake has hit top
+    ) {
+        return clearInterval(timerId)
+    }
+
+
+
     //remove last element from our currentSnake array
     const tail = currentSnake.pop()
     //remove styling from the last element
@@ -29,13 +44,9 @@ function move() {
     //add styling so we can see it
     squares[currentSnake[0]].classList.add('snake')
 }
-
-move()
-
 let timerId = setInterval(move, 1000)
 
-
-document.addEventListener("keyup", (e) => {
+function control(e) {
     if (e.keyCode === 39) {
         direction = 1
     } else if (e.keyCode === 38) {
@@ -45,5 +56,6 @@ document.addEventListener("keyup", (e) => {
     } else if (e.keyCode === 40) {
         direction = 10
     }
-})
+}
+document.addEventListener("keydown", control)
 // clearInterval(timerId)
